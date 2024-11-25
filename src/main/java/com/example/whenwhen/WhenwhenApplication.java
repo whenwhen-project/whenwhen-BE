20ed777c839e979992a3269b63f36c9e82bcbb08
j30ngwoo <ttangle1234@naver.com>
2024-11-25 14:47:50 +0900

    project init

---
+++ src/main/java/com/example/whenwhen/WhenwhenApplication.java
@@ -0,0 +1,13 @@
+package com.example.whenwhen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhenwhenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhenwhenApplication.class, args);
+	}
+
+}
