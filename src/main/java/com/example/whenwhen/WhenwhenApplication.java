45d19a483c4463b0ab99a30f9944f3d23ab475f2
j30ngwoo <ttangle1234@naver.com>
2024-12-06 01:42:52 +0900

    스프링 시큐리티 로그인 비활성화

--- src/main/java/com/example/whenwhen/WhenwhenApplication.java
+++ src/main/java/com/example/whenwhen/WhenwhenApplication.java
@@ -2,12 +2,12 @@ package com.example.whenwhen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WhenwhenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WhenwhenApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WhenwhenApplication.class, args);
+    }
 }
