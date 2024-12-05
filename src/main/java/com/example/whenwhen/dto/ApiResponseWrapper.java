e193e8d1be44600a5b127e18a468f839c49db26c
j30ngwoo <ttangle1234@naver.com>
2024-12-06 01:43:57 +0900

    response API 규격화를 위한 Wrapper 추가

---
+++ src/main/java/com/example/whenwhen/dto/ApiResponseWrapper.java
@@ -0,0 +1,20 @@
+package com.example.whenwhen.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Schema(description = "API 공통 응답 형식")
+public class ApiResponseWrapper<T> {
+
+    @Schema(description = "요청 성공 여부", example = "true")
+    private boolean success;
+
+    @Schema(description = "응답 메시지", example = "Event created successfully.")
+    private String message;
+
+    @Schema(description = "응답 데이터")
+    private T data;
+}
