e9cb54438d10efef5212d3ce94cfdc21006f8a76
j30ngwoo <ttangle1234@naver.com>
2024-12-31 01:58:21 +0900

    Auth 구현

---
+++ src/main/java/com/example/whenwhen/config/AuthControllerDocs.java
@@ -0,0 +1,44 @@
+package com.example.whenwhen.config;
+
+import com.example.whenwhen.dto.ApiResponseWrapper;
+import com.example.whenwhen.dto.EventRequestDto;
+import com.example.whenwhen.dto.EventResponseDto;
+import com.example.whenwhen.dto.LoginResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Tag(name = "[Auth API]")
+public interface AuthControllerDocs {
+    @Operation(
+            summary = "카카오 로그인 URL 리다이렉트",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "302",
+                            description = "카카오 로그인 URL로 302 Redirect",
+                            headers = {
+                                    @Header(name = "Location", schema = @Schema(type = "string"))
+                            }
+                    )
+            }
+    )
+    public ResponseEntity<?> redirectKakaoLoginUrl();
+
+    @Operation(
+            summary = "카카오로 로그인 혹은 회원가입",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "로그인/회원가입 성공",
+                            useReturnTypeSchema = true
+                    ),
+            }
+    )
+    public ResponseEntity<?> handleKakaoCallback(@RequestParam String code);
+}
---
+++ src/main/java/com/example/whenwhen/service/KakaoAuthProvider.java
@@ -0,0 +1,71 @@
+package com.example.whenwhen.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class KakaoAuthProvider {
+
+    private final WebClient webClient;
+
+    @Value("${kakao.auth.client-id}")
+    private String clientId;
+
+    @Value("${kakao.auth.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${kakao.auth.scope}")
+    private String scope;
+
+    public String getKakaoLoginRedirectUrl() {
+        return "https://kauth.kakao.com/oauth/authorize"
+                + "?response_type=code"
+                + "&client_id=" + clientId
+                + "&redirect_uri=" + redirectUri
+                + "&scope=" + scope;
+    }
+
+    // TODO: Map Warning 수정하기
+
+    /**
+     * 카카오로부터 액세스 토큰과 ID 토큰 등 획득
+     * @param code 카카오 OAuth에서 받은 인가 코드
+     * @return 액세스 토큰, ID 토큰 등
+     */
+    public Map<String, Object> getKakaoToken(String code) {
+        return webClient.post()
+                .uri("https://kauth.kakao.com/oauth/token")
+                .header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+                .bodyValue("grant_type=authorization_code"
+                        + "&client_id=" + clientId
+                        + "&redirect_uri=" + redirectUri
+                        + "&code=" + code)
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+    }
+
+    /**
+     * 카카오 액세스 토큰에 있는 ID 토큰을 디코딩하고 사용자 정보 반환
+     * @param idToken 카카오 ID 토큰
+     * @return Map 형식의 사용자 정보. sub, nickname, picture 등
+     */
+    public Map<String, Object> decodeIdTokenToUserInfo(String idToken) {
+        try {
+            String[] parts = idToken.split("\\.");
+            String payload = new String(Base64.getUrlDecoder().decode(parts[1]), StandardCharsets.UTF_8);
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(payload, Map.class);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Invalid ID token", e);
+        }
+    }
+}
