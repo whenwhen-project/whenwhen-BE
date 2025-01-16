9a3cd36540873b8d225f4f644c757622395bd4a2
j30ngwoo <ttangle1234@naver.com>
2025-01-16 15:04:09 +0900

    EntityNotFoundException 삭제

--- src/main/java/com/example/whenwhen/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.whenwhen.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
